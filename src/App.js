ba8fafd383b978e7a68ee0465d642d6770a424a7
mendelB <mendelblesofsky@gmail.com>
2017-08-07 17:57:46 -0400

    use apiclient, add get new quiz button

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import React, { Component } from 'react';
 import Question from './components/Question';
+import quizClient from './utils/quizClient';
 import logo from './logo.svg';
 import './App.css';
-
-const questionDummy = {
-  question: "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
-  "correct_answer": 0,
-  choices: ["Harry Potter","Ted","Spy Kids","Pirates of the Caribbean "]
-}
-
-const q2 = {
-  "question": "When was the movie &#039;Con Air&#039; released?",
-  "correct_answer": 0,
-  "choices": [
-    "1997",
-    "1985",
-    "1999",
-    "1990"
-  ]
-}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: [questionDummy, q2],
+      questions: [],
       currQuestion: 0,
       score: 0
     }
 
     this.handleClickNext = this.handleClickNext.bind(this)
     this.updateScore = this.updateScore.bind(this)
+    this.getNewQuiz = this.getNewQuiz.bind(this)
+  }
+
+  getNewQuiz() {
+    quizClient.getQuizes().then(questions => {
+      this.setState({questions: questions, score: 0})
+    })
+  }
+
+  componentDidMount() {
+    this.getNewQuiz()
   }
 
   handleClickNext() {
@@ -43,15 +38,17 @@ class App extends Component {
 
   render() {
     const nextQuestion = this.state.currQuestion < this.state.questions.length - 1
-    const question = this.state.questions[this.state.currQuestion]
+    const question = this.state.questions[this.state.currQuestion] 
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to Quiz!</h2>
+          <button className="btn btn-default" onClick={this.getNewQuiz}>Get New Quiz</button>
         </div>
         <div className="App-intro">
           <h3>Score: {this.state.score}/{this.state.questions.length}</h3>
-          <Question question={question.question} choices={question.choices} correctChoice={question.correct_answer} nextQuestion={nextQuestion} handleClickNext={this.handleClickNext} updateScore={this.updateScore}/>
+          <h3>Question: {this.state.currQuestion + 1}/{this.state.questions.length}</h3>
+          {question ? <Question question={question.question} choices={question.choices} correctChoice={question.correctChoice} nextQuestion={nextQuestion} handleClickNext={this.handleClickNext} updateScore={this.updateScore}/> : null}
         </div>
       </div>
     );
