378e712b29c13c72c47198fbe1ba595089d4678e
mendelB <mendelblesofsky@gmail.com>
2017-08-07 17:08:16 -0400

    keep track of curr question, render next question once next button clicked

--- src/App.js
+++ src/App.js
@@ -5,19 +5,46 @@ import './App.css';
 
 const questionDummy = {
   question: "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
+  "correct_answer": 0,
   choices: ["Harry Potter","Ted","Spy Kids","Pirates of the Caribbean "]
 }
 
+const q2 = {
+  "question": "When was the movie &#039;Con Air&#039; released?",
+  "correct_answer": 0,
+  "choices": [
+    "1997",
+    "1985",
+    "1999",
+    "1990"
+  ]
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [questionDummy, q2],
+      currQuestion: 0,
+    }
+
+    this.handleClickNext = this.handleClickNext.bind(this)
+  }
+
+  handleClickNext() {
+    this.setState({currQuestion: this.state.currQuestion+1})
+  }
+
   render() {
+    const nextQuestion = this.state.currQuestion < this.state.questions.length - 1
+    const question = this.state.questions[this.state.currQuestion]
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Quiz!</h2>
         </div>
         <div className="App-intro">
-          <Question question={questionDummy.question} choices={questionDummy.choices} correctChoice={0}/>
+          <Question question={question.question} choices={question.choices} correctChoice={question.correct_answer} nextQuestion={nextQuestion} handleClickNext={this.handleClickNext}/>
         </div>
       </div>
     );
