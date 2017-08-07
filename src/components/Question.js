08bccd71d891fdef9354608f5628221b4c78556e
mendelB <mendelblesofsky@gmail.com>
2017-08-07 15:13:35 -0400

    add click handler, and render all choices

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import Choice from './Choice';
 
 class Question extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick(index) {
+		const choice = this.props.choices[index]
+	}
+
 	render() {
 		return (
-			<h3>{this.props.question}</h3>		 
+			<div>
+				<h3>{this.props.question}</h3>
+				<ol>
+					{
+						this.props.choices.map((choice,index) => 
+							<Choice 
+								key={index}
+								choice={choice}
+								handleClick={this.handleClick.bind(index)}
+							/>
+						)
+					}
+				</ol>		 
+			</div>
 		)
 	}
 }
