8d03cb3d791039782db558ccfa78a58c531ea2a5
mendelB <mendelblesofsky@gmail.com>
2017-08-07 15:05:05 -0400

    fix extends

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Question extends React.component {
+class Question extends Component {
 	render() {
 		return (
 			<h3>{this.props.question}</h3>		 
