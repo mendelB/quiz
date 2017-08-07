1d9e744d1058788183843b07b9a829ccd6918f63
mendelB <mendelblesofsky@gmail.com>
2017-08-07 16:32:50 -0400

    add states and css classes through props

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Choice = (props) => {
+	const classStates = {
+		'active': "list-group-item-action",
+		'correct': "list-group-item-success",
+		'incorrect': "list-group-item-danger",
+		'inactive': "disabled"
+	}
 	return (
 		<button 
-			className="list-group-item list-group-item-action" 
-			onClick={props.handleClick}>
+			className={"list-group-item " + classStates[props.activeState]}
+			onClick={props.handleClick}
+			disabled={props.disabled}>
 				{props.choice}
 		</button>	 
 	)
