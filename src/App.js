24db0ab0baa8b15cb9a51389d02d05d8a85c7094
mendelB <mendelblesofsky@gmail.com>
2017-08-08 11:01:57 -0400

    remove logo import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Question from './components/Question';
 import quizClient from './utils/quizClient';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
