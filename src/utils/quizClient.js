bdc2cdd797e51586ba99140e002400f0714b753a
mendelB <mendelblesofsky@gmail.com>
2017-08-08 11:48:45 -0400

    use js decodeURI, remove old functions

--- src/utils/quizClient.js
+++ src/utils/quizClient.js
@@ -1,20 +1,13 @@
-const url = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple&encode=base64"
-
-function b64DecodeUnicode(str) {
-    // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
+const url = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple&encode=url3986"
 
 const makeQuizObjs = (json) => {
 		return json.results.map(question => {
 			const q = {
-				question: b64DecodeUnicode(question.question),
-				choices: question.incorrect_answers.map(c => b64DecodeUnicode(c)),
+				question: decodeURIComponent(question.question),
+				choices: question.incorrect_answers.map(c => decodeURIComponent(c)),
 			}
 			const correct_answer_index = Math.floor(Math.random() * (q.choices.length - 1) + 1);
-			q.choices.splice(correct_answer_index, 0, b64DecodeUnicode(question.correct_answer))
+			q.choices.splice(correct_answer_index, 0, decodeURIComponent(question.correct_answer))
 			q.correctChoice = correct_answer_index
 			return q
 		})
